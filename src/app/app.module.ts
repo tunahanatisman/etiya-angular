6989771b8a204f254c60e852843eef253d5374c9
tuna676 <67320832+tuna676@users.noreply.github.com>
2022-07-27 10:33:52 +0300

    etiya-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddProductComponent } from './pages/add-product/add-product.component';
+import { FormsModule } from '@angular/forms';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ProductListComponent, AddProductComponent, HomepageComponent, NavbarComponent, ProductCardComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
